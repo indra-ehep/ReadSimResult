47e22ebcb330f8095f5fd808426e78aa95982f8d
Indranil Das <indranil.das@cern.ch>
2021-12-04 09:28:29 +0530

    Adding Simhit analysis code

--- HitAnalysis/scripts/runloop.sh
+++ HitAnalysis/scripts/runloop.sh
@@ -4,7 +4,7 @@
 #inputdir=/eos/user/i/idas/SimOut/DeltaPt/Extended2026D86
 inputdir=/eos/user/p/psuryade/SimOut/DeltaPt/Extended2026D86_25
 pydir=$PWD/ReadSimResult/SimTrackAna/python
-for i in `seq 0 9`
+for i in `seq 0 2`
 do
   echo Processing loop $i with file step1_${i}.root
   # if [ -f $inputdir/step1.root ] ; then
