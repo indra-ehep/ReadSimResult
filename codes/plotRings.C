5009808f38d2b8f53c2806a3eb2607166abd7b21
Indranil Das <indranil.das@cern.ch>
2022-09-21 18:48:20 +0530

    updating scintillator plotting with shift

--- codes/plotRings.C
+++ codes/plotRings.C
@@ -13,7 +13,13 @@ int GetCartesian(double r, double theta, double& x, double& y){
   return true;
 }
 
-int plotRings(int refLayer=34)
+int GetPolar(double x, double y, double& r, double& theta){
+  r = TMath::Sqrt(x*x + y*y);
+  theta =  180.0*TMath::ATan2(y, x)/TMath::Pi();
+  return true;
+}
+
+int plotRings(int refLayer=47)
 {
   
   ifstream fin("Validation/HGCalValidation/data/scintillatorV0.txt");
@@ -29,29 +35,36 @@ int plotRings(int refLayer=34)
   // TFile *fGeant = TFile::Open("geantoutput_merged_D49.root");
   // TH2D *hXYhits = (TH2D *)fGeant->Get(Form("prodHEB/hXYhits_layer_%d",refLayer));
 
-  TFile *fGeant = TFile::Open("geantoutputD88_gr-merged_old_cmssw.root");
-  TH2D *hXYhits = (TH2D *)fGeant->Get(Form("hgcalCellHitSumHEB/hXYhits_layer_%d",refLayer));
+  // TFile *fGeant = TFile::Open("geantoutputD88_gr-merged_ReEval.root");
+  // TH2D *hXYhits = (TH2D *)fGeant->Get(Form("hgcalCellHitSumHEB/hXYhits_layer_%d",refLayer));
   
-  TCanvas *c1 = new TCanvas("c1","c1",600,600);
-  gStyle->SetOptStat(100011);
-  hXYhits->SetMarkerColor(kBlue);
-  hXYhits->Draw("sames");
-
-  // TFile *fGeant = TFile::Open("geantoutputD88_gr-merged_old_cmssw.root");
-  // TGraph *hXYhits = (TGraph *)fGeant->Get(Form("hgcalCellHitSumHEB/gXYhitsB1_layer_%d",refLayer));
   // TCanvas *c1 = new TCanvas("c1","c1",600,600);
-  // hXYhits->SetTitle(Form("Hits in layer %d (z > 0.0 cm)",refLayer));
+  // gStyle->SetOptStat(100011);
   // hXYhits->SetMarkerColor(kBlue);
-  // hXYhits->SetMarkerStyle(kFullCircle);
-  // hXYhits->SetMarkerSize(0.1);
-  // hXYhits->Draw("AP");
+  // hXYhits->Draw("sames");
 
+  
+  //TFile *fGeant = TFile::Open("geantoutputD92_val_cassette_shift.root");
+  TFile *fGeant = TFile::Open("geantoutputD92_gr-merged.root");
+  TGraph *hXYhits = (TGraph *)fGeant->Get(Form("hgcalCellHitSumHEB/gXYhitsB0_layer_%d",refLayer));
+  //TGraph *hXYhits = (TGraph *)fGeant->Get(Form("hgcalCellHitSumHEF/gXYhitsCK1_layer_%d",refLayer));
+  TCanvas *c1 = new TCanvas("c1","c1",600,600);
+  hXYhits->SetTitle(Form("Hits in layer %d (z > 0.0 cm)",refLayer));
+  hXYhits->SetMarkerColor(kBlue);
+  //hXYhits->SetMarkerColor(kMagenta);
+  hXYhits->SetMarkerStyle(kFullCircle);
+  hXYhits->SetMarkerSize(0.1);
+  hXYhits->Draw("AP");
+
+  float xshift = 60.0, yshift = 60.0;
+  int tcassette = 1;
   for(int ilir=0;ilir<SciLayouts.size();ilir++){
     int layer, ring;
     float rmin, rmax, sipmsize;
     long int hex1, hex2, hex3, hex4;
     string hexcomb;
     char scitype;
+    int icassette = 0 ;
     sscanf((const char *)SciLayouts.at(ilir).c_str(), "%d %d %f %f %f %lX %lX %lX %lX %c\n", &layer, &ring, &rmin, &rmax, &sipmsize, &hex1, &hex2, &hex3, &hex4, &scitype);
     if(layer==refLayer){
       printf("%d %d %6.2f %6.2f %3.1f %lX %lX %lX %lX %c\n",  layer,  ring,  rmin,  rmax,  sipmsize,  hex1,  hex2,  hex3,  hex4,  scitype);
@@ -61,24 +74,61 @@ int plotRings(int refLayer=34)
       cout<<hexcomb<<endl;
       rmax /= 10.0;
       rmin /= 10.0;
+      // xshift /= 10.0;
+      // yshift /= 10.0;
       //if(ring==29){
+      int iseg = 0 ;
       for(int isect=0;isect<3;isect++){
+	cout<<" hexcomb.size() : " << hexcomb.size() << endl;
 	for(int iphi=0;iphi<hexcomb.size();iphi++){
-	  float phimin = isect*120. + iphi*1.25;
-	  float phimax = isect*120. + (iphi+1)*1.25;
-	  bool isDraw = (hexcomb[iphi]=='1') ? true : false; 
+
+	  double phimin = isect*120. + iphi*1.25;
+	  double phimax = isect*120. + (iphi+1)*1.25;
+	  double nrmin = rmin ;
+	  double nrmax = rmax ;
+	  
+	  if(iseg%24==0)
+	    icassette++;
+	  //cout << "LProcesing cassette : " <<icassette<<endl;
+	  double offsetX = 0.0;
+	  double offsetY = 0.0;
+	  double offsetR = 0.0;
+	  double offsetT = 0.0;
+	  if(icassette==tcassette){
+	    offsetX = xshift;
+	    offsetY = yshift;
+	    double x1,y1,x2,y2,x3,y3,x4,y4;
+	    GetCartesian(rmin, phimin, x1, y1); x1 += xshift; y1 += yshift; 
+	    GetCartesian(rmin, phimax, x2, y2); x2 += xshift; y2 += yshift; 
+	    GetCartesian(rmax, phimin, x3, y3); x3 += xshift; y3 += yshift; 
+	    GetCartesian(rmax, phimax, x4, y4); x4 += xshift; y4 += yshift; 
+	    
+	    GetPolar(x1, y1, nrmin, phimin);
+	    GetPolar(x2, y2, nrmin, phimax);
+	    GetPolar(x3, y3, nrmax, phimin);
+	    GetPolar(x4, y4, nrmax, phimax);
+	    
+	    GetPolar(offsetX, offsetY, offsetR, offsetT);
+	    cout << "offsetX " << offsetX << ", offsetY " << offsetY << ", offsetR " << offsetR << ", offsetT " << offsetT << endl;
+	  }else{
+	    offsetX = 0.0;
+	    offsetY = 0.0;
+	  }
+	  
+	  
+	  bool isDraw = (hexcomb[iphi]=='1') ? true : false;
 	  if(isDraw){
 	    
-	    TArc *arc1 = new TArc(0.0, 0.0, rmax, phimin,phimax);
+	    TArc *arc1 = new TArc(0.0, 0.0, nrmax, phimin,phimax);
 	    arc1->SetFillStyle(0);
 	    arc1->SetNoEdges();
 	    arc1->SetLineWidth(1);
 	    arc1->Draw("sames");
 	    
 	    double x1,y1,x2,y2;
-	    GetCartesian(rmax, phimin, x1, y1);
+	    GetCartesian(nrmax, phimin, x1, y1);
 	    // printf("x1,y1 : %f,%f\n",x1,y1);
-	    GetCartesian(rmin, phimin, x2, y2);
+	    GetCartesian(nrmin, phimin, x2, y2);
 	    // printf("x2,y2 : %f,%f\n",x2,y2);
 	    TLine *l1 = new TLine(x1,y1,x2,y2);
 	    //l1->SetLineColor(kBlue);
@@ -86,9 +136,9 @@ int plotRings(int refLayer=34)
 	    l1->SetLineWidth(1);
 	    l1->Draw("sames");
 
-	    GetCartesian(rmax, phimax, x1, y1);
+	    GetCartesian(nrmax, phimax, x1, y1);
 	    // printf("x1,y1 : %f,%f\n",x1,y1);
-	    GetCartesian(rmin, phimax, x2, y2);
+	    GetCartesian(nrmin, phimax, x2, y2);
 	    // printf("x2,y2 : %f,%f\n",x2,y2);
 	    TLine *l2 = new TLine(x1,y1,x2,y2);
 	    //l1->SetLineColor(kBlue);
@@ -96,13 +146,14 @@ int plotRings(int refLayer=34)
 	    l2->SetLineWidth(1);
 	    l2->Draw("sames");
 
-	    TArc *arc2 = new TArc(0.0, 0.0, rmin, phimin,phimax);
+	    TArc *arc2 = new TArc(0.0, 0.0, nrmin, phimin,phimax);
 	    arc2->SetFillStyle(0);
 	    arc2->SetNoEdges();
 	    arc2->SetLineWidth(1);
 	    arc2->Draw("sames");
 	    
-	  }
+	  }//isDraw
+	  iseg++;
 	}
       }
     }
@@ -116,6 +167,8 @@ int plotRings(int refLayer=34)
   hXYhits->GetYaxis()->SetTitle("y (cm)");
   c1->SaveAs(Form("Scintillator_merged_Layer_%d.png",refLayer));
   c1->SaveAs(Form("Scintillator_merged_Layer_%d.pdf",refLayer));
+  //c1->SaveAs(Form("Silicon_merged_Layer_%d.png",refLayer));
+  //c1->SaveAs(Form("Silicon_merged_Layer_%d.pdf",refLayer));
   //c1->SaveAs(Form("Scintillator_merged_Layer_%d.svg",refLayer));
   delete fGeant;
   
